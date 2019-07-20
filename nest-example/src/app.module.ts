a39b291fa827176935f5d93c680c28bcb48f9d31
fanbaolin <fanbaolin@codemao.cn>
2019-07-20 20:00:53 +0800

    mongo pwd

--- nest-example/src/app.module.ts
+++ nest-example/src/app.module.ts
@@ -4,7 +4,7 @@ import { TeamModule } from './team/team.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://mongoroot:mongopwd@123.207.9.26:21087/nest'),
+    MongooseModule.forRoot('mongodb://mongoroot:mongopwd@127.0.0.1:21087/nest'),
     TeamModule,
   ],
 })
